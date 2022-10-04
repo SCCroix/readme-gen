54f2641a74eadf8515066ca65765f5217351ecb7
Spencer Croy <spencerccroy@gmail.com>
2022-10-03 22:10:14 -0700

    Add simple file structure

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const { writeFile } = require('fs').promises;
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is your What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'What is a description of your project?',
+      },
+      {
+        type: 'input',
+        name: 'installInstructions',
+        message: 'How is your app installed?',
+      },
+      {
+        type: 'input',
+        name: 'usageInstructions',
+        message: 'What are the usage instructions',
+      },
+      {
+        type: 'input',
+        name: 'contributionGuidelines',
+        message: 'What are the contribution Guidelines for your app?',
+      },
+      {
+        type: 'input',
+        name: 'testInstructions',
+        message: 'what are the tests for this app',
+      },
+    ]);
+  };
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
